591086723a4924dbcceab0a1c55159c0fed5f6a8
OGdeveloper-AI <83118799+OGdeveloper-AI@users.noreply.github.com>
2021-07-09 16:25:58 +0400

    Add files via upload

---
+++ Positive_Numbers.py
@@ -0,0 +1,10 @@
+list1 = [12, -7, 5, 64, -14]
+for number in list1:
+    if number >= 0:
+       print("Positive number in list 1: ", number)
+       
+list2 = [12, 14, -95, 3]
+for number in list2:
+    if number >= 0:
+       print("Positive number in list 2: ", number)
+       
